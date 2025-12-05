b266497c221a235a89b32170c552668d62b5bdee
Alexo <alexsuciultc200@gmail.com>
2025-12-05 17:15:31 +0200

    project setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+
+const allowedOrigin = process.env.ALLOWED_ORIGIN;
+
+const corsOptions = {
+    origin: allowedOrigin,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'username'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    if (req.url === '/' && req.method === 'GET') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('InfoBase0');
+    }
+    next();
+})
+mongoose.connect(process.env.mongoDB)
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((error) => {
+    console.error("MongoDB connection failed:", error);
+}); 
+
+server.listen(process.env.PORT || 4000, () => {
+    console.log('Server listening on port', process.env.PORT || 4000);
+});
